073a60395dc31e3b7f2c864a0bf24541c4ba1c82
arian81 <35879206+arian81@users.noreply.github.com>
2022-05-24 21:38:14 -0400

    changed to new team

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,28 +1,33 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 
 import { device } from "../device";
 import ContentWrapper from "./ContentWrapper";
 
+import omar from "../assets/team/omar.jpg";
+import geon from "../assets/team/geon.jpg";
+import haley from "../assets/team/haley.jpg";
+import youcef from "../assets/team/youcef.jpg";
+import insiyah from "../assets/team/insiyah.jpg";
+import nihal from "../assets/team/nihal.jpg";
+import proyetei from "../assets/team/proyetei.jpg";
+import arian from "../assets/team/arian.jpg";
 import placeholder from "../assets/team/placeholder.jpg";
-import ali from "../assets/team/ali.jpg";
-import jerry from "../assets/team/jerry.jpg";
-import khizar from "../assets/team/khizar.jpg";
-import nida from "../assets/team/nida.jpg";
+import arruran from "../assets/team/arruran.jpg";
 
 const TeamContentWrapper = styled(ContentWrapper)`
-    display:flex;
+    display: flex;
     flex-wrap: wrap;
     align-items: center;
     justify-content: space-evenly;
     column-gap: 100px;
-    row-gap:50px;
+    row-gap: 50px;
     margin-bottom: 50px;
 `;
 
 const Person = styled.img`
-    width: 200px;
-    border-radius: 49%;
+    width: 150px;
+    border-radius: 50%;
     margin-bottom: 10px;
 `;
 
@@ -49,50 +54,74 @@ const Title = styled.h1`
 
 const Team = () => (
     <div>
-         <Title>Our Team</Title>
-    <TeamContentWrapper>
-       
-        <PersonContainer>
-            <Person src={nida}/>
-            <Name>Nida Nasir</Name>
-            <Position>President</Position>
-        </PersonContainer>
-        <PersonContainer>
-            <Person src={ali}/>
-            <Name>Ali Shahpurwala</Name>
-            <Position>Vice President</Position>
-        </PersonContainer>
-        <PersonContainer>
-            <Person src={khizar}/>
-            <Name>Khizar Siddiqui</Name>
-            <Position>Director of Marketing</Position>
-        </PersonContainer>
-        <PersonContainer>
-            <Person src={placeholder}/>
-            <Name>Naga Sasi Dhar</Name>
-            <Position>Director of Administration</Position>
-        </PersonContainer>
-        <PersonContainer>
-            <Person src={placeholder}/>
-            <Name>Omar Alkersh</Name>
-            <Position>Director of Academics</Position>
-        </PersonContainer>
-        <PersonContainer>
-            <Person src={placeholder}/>
-            <Name>Sammy Dods</Name>
-            <Position>Director of Student Affairs</Position>
-        </PersonContainer>
-        <PersonContainer>
-            <Person src={placeholder}/>
-            <Name>Ibtehaaj Cheema</Name>
-            <Position>Director of Finance</Position>
-        </PersonContainer>
-        <PersonContainer>
-            <Person src={jerry}/>
-            <Name>Jerry Li</Name>
-            <Position>Director of Technology</Position>
-        </PersonContainer>
-    </TeamContentWrapper>
+        <Title>Our Team</Title>
+        <TeamContentWrapper>
+            <PersonContainer>
+                <Person src={omar} />
+                <Name>Omar Alkersh</Name>
+                <Position>President</Position>
+            </PersonContainer>
+            <PersonContainer>
+                <Person src={arian} />
+                <Name>Arian Ahmadinejad</Name>
+                <Position>Technical Lead</Position>
+            </PersonContainer>
+            <PersonContainer>
+                <Person src={geon} />
+                <Name>Geon Youn</Name>
+                <Position>Director of Student Affairs</Position>
+            </PersonContainer>
+            <PersonContainer>
+                <Person src={nihal} />
+                <Name>Nihal Singh</Name>
+                <Position>Director of Academics</Position>
+            </PersonContainer>
+            <PersonContainer>
+                <Person src={proyetei} />
+                <Name>Proyetei Akanda</Name>
+                <Position>Director of Marketing</Position>
+            </PersonContainer>
+            <PersonContainer>
+                <Person src={youcef} />
+                <Name>Youcef Boumar</Name>
+                <Position>Academic Manager</Position>
+            </PersonContainer>
+            <PersonContainer>
+                <Person src={haley} />
+                <Name>Haley Johnson</Name>
+                <Position>Social Media Manager</Position>
+            </PersonContainer>
+            <PersonContainer>
+                <Person src={insiyah} />
+                <Name>Insiyah Ujjainwala</Name>
+                <Position>Social Media Manager</Position>
+            </PersonContainer>
+            <PersonContainer>
+                <Person src={placeholder} />
+                <Name>Esha Pisharody</Name>
+                <Position>Director of Administration</Position>
+            </PersonContainer>
+            <PersonContainer>
+                <Person src={arruran} />
+                <Name>Arruran Kamalavannan</Name>
+                <Position>Director of Marketing</Position>
+            </PersonContainer>
+            <PersonContainer>
+                <Person src={placeholder} />
+                <Name>Sohaib Ahmed</Name>
+                <Position>Director of Marketing</Position>
+            </PersonContainer>
+            <PersonContainer>
+                <Person src={placeholder} />
+                <Name>Mohammed Haq</Name>
+                <Position>Graphics Designer</Position>
+            </PersonContainer>
+            <PersonContainer>
+                <Person src={placeholder} />
+                <Name>Khai</Name>
+                <Position>Graphics Designer</Position>
+            </PersonContainer>
+        </TeamContentWrapper>
     </div>
 );
 
